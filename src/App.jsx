89bee8eb91072ad58f99de5592d7903f8ee4d6e0
sofinavas <sofianavas.g@gmail.com>
2023-02-11 21:56:06 -0300

    header + ruteos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import Welcome from "./components/Welcome";
+import Cart from "./components/Cart";
+
+
 
 
 
 function App() {
   
 return (
-  <>
-    
-    <NavBar/>
-    <ItemListContainer/>
-  </>
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      
+      <Route exact path = "/" element= {<Welcome/>} />
+      <Route exact path = "/Catalogue" element= {<ItemListContainer/>} />
+      
+      <Route exact path = "/Cart" element= {<Cart/>} />
+
+      
+      
+      
+
+    </Routes>
+
+  
+  
+  </BrowserRouter>
 
 );
   
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,80 @@
 import React from 'react'
 import CartWidget from './CartWidget';
-import { Container, Flex, Spacer, Box, Heading } from '@chakra-ui/react';
+import { Container, Flex, Spacer, Box, Heading, Menu, Button, MenuList, MenuButton, MenuItem, } from '@chakra-ui/react';
+import logo from '../images/logo.jpg';
+import { Link } from 'react-router-dom';
 
 
 function NavBar(props) {
   return (
     <>
-         <Container maxW= "-moz-max-content" bg='blue.200' color='white'>
+         <Container maxW= "-moz-max-content" className="navBar">
             <Flex alignContent="center" gap="2">
-                <Box p="2">
-                    <Heading size="md"> E-Commerce</Heading>
+                
+                <Box> 
+                  <Heading >
+                    <Link to={"/"}>
+                      <img src={logo} alt="Logo" className="logo" />
+                    </Link>
+                  </Heading>
                 </Box>
 
                 <Spacer/>
 
-                <Box p="2">
-                <CartWidget/>
+                <Box>
+                    <Menu>
+                      <Link to={"/catalogue"}>   
+                        <MenuButton  as={Button} size="lg"
+                                      variant="outline"
+                                      colorScheme="Orange 50"
+                                      m="5">
+                            Menue
+                        </MenuButton>
+                      </Link>
+                    </Menu>
+                </Box>
+                  
+
+                <Spacer/>
+          
+                <Box>
+                    <Menu>
+                            <MenuButton
+                                        as={Button}
+                                        size="lg"
+                                        variant="outline"
+                                        colorScheme="Orange 50"
+                                        m="5">
+                            Categories
+                            </MenuButton>
+
+                            <MenuList color="red">
+                                        
+                                
+                                  <MenuItem>Pizzas</MenuItem>
+                                
+                                
+                                  <MenuItem>Desserts</MenuItem>
+                                
+                                
+                                  <MenuItem>Refreshing</MenuItem>
+                               
+                            </MenuList>
+
+
+                    </Menu>
+                  
+                    
+
+                </Box>
+
+
+                <Spacer/>
+
+                <Box p="10" size="xls">
+                    <Link to={"/cart"}>
+                      <CartWidget />
+                    </Link>
                 </Box>
                 
             </Flex>
